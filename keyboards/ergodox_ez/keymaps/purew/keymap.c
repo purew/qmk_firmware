bf23cd426d4b50793e9fbb26f063ebf8f356a812
Anders Bennehag <anders@bennehag.com>
2025-11-08 08:01:56 -0700

    First dvorak attempt

--- keyboards/ergodox_ez/keymaps/purew/keymap.c
+++ keyboards/ergodox_ez/keymaps/purew/keymap.c
@@ -36,14 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    HYPR_T(KC_NO),                 MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
+  KC_GRV,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
+  KC_DEL,          GUI_T(KC_QUOT),        KC_COMM,          KC_DOT,    KC_P,    KC_Y,    TG(SYMB),             TG(SYMB),     KC_F,    KC_G,    KC_C,    KC_R,              KC_L,           KC_EQL,
+  KC_BSPC,         KC_A,        KC_O,          KC_E,    KC_U,    KC_I,                                        KC_D,    KC_H,    KC_T,    KC_N,    LT(MDIA, KC_S), GUI_T(KC_MINS),
+  KC_LSFT,         CTL_T(KC_SCLN), KC_Q,          KC_J,    KC_K,    KC_X,    HYPR_T(KC_NO),                 MEH_T(KC_SLSH), KC_B,    KC_M,    KC_W, KC_V,           CTL_T(KC_Z), KC_RSFT,
   LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
                                                            ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
                                                          KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
+
+    // TODO: KC_BSLS, GUI_T(KC_QUOT), LT(MDIA, KC_SCLN)
+    //
 ),
 /* Keymap 1: Symbol Layer
  *
